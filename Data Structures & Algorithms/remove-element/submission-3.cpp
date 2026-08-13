a45ee83076ebef1601db9045c4b886c7e38b7b2b
Oussama Ayari <oussama.ayari@mines-albi.fr>
2026-08-13 15:21:54 +0200

    Add: remove-element - submission-3

---
+++ Data Structures & Algorithms/remove-element/submission-3.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k=0;
+
+        for(int n : nums){
+            if(n!=val)
+                nums[k++]=n;
+        }
+
+        return k;
+    }
+};
